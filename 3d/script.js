3880e4e97b4ef1beff90ce61ddbf01898359b345
Penguint <tjpyytqz@outlook.com>
2017-04-04 19:19:08 +0800

    Beautify JS

--- 3d/script.js
+++ 3d/script.js
@@ -5,54 +5,54 @@ var isLoaded = false;
 
 var elt = document.getElementById('calculator');
 var calculator = Desmos.GraphingCalculator(elt, {
-    border: false
+	border: false
 });
 
 var uMin, uMax, vMin, vMax;
 
 function update() {
-    state = calculator.getState();
-    explist = state.expressions.list;
-
-    uMin = explist[15].latex;
-    uMax = explist[16].latex;
-    vMin = explist[20].latex;
-    vMax = explist[21].latex;
-
-    calculator.setExpression({
-        id: 'border.u',
-        domain: {
-            min: uMin,
-            max: uMax
-        }
-    });
-    calculator.setExpression({
-        id: 'border.v',
-        domain: {
-            min: vMin,
-            max: vMax
-        }
-    });
+	state = calculator.getState();
+	explist = state.expressions.list;
+	
+	uMin = explist[15].latex;
+	uMax = explist[16].latex;
+	vMin = explist[20].latex;
+	vMax = explist[21].latex;
+	
+	calculator.setExpression({
+		id: 'border.u',
+		domain: {
+			min: uMin,
+			max: uMax
+		}
+	});
+	calculator.setExpression({
+		id: 'border.v',
+		domain: {
+			min: vMin,
+			max: vMax
+		}
+	});
 }
 
 function loadini() {
-    $.getJSON('./inistate.json', function(state) {
-        calculator.setDefaultState(state);
-        calculator.setState(state);
-
-        $('.loading-container').hide();
-        isLoaded = true;
-    });
+	$.getJSON('./inistate.json', function(state) {
+		calculator.setDefaultState(state);
+		calculator.setState(state);
+		
+		$('.loading-container').hide();
+		isLoaded = true;
+	});
 }
 
 window.onload = function() {
-    if (!isLoaded) {
-        loadini();
-
-        calculator.observeEvent('change', function() {
-            if (isLoaded) {
-                update();
-            }
-        });
-    }
+	if (!isLoaded) {
+		loadini();
+		
+		calculator.observeEvent('change', function() {
+			if (isLoaded) {
+				update();
+			}
+		});
+	}
 };
